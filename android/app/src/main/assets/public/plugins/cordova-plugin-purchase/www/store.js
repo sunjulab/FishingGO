f29d92bb184e02ac93d2d29a49b2412913d26586
FishingGO Dev <dev@fishinggo.com>
2026-09-09 22:14:42 +0900

    chore: 배포 자동화 커밋 (deploy-all)

--- android/app/src/main/assets/public/plugins/cordova-plugin-purchase/www/store.js
+++ android/app/src/main/assets/public/plugins/cordova-plugin-purchase/www/store.js
@@ -1,3 +1,4 @@
+cordova.define("cordova-plugin-purchase.CdvPurchase", function(require, exports, module) { 
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -9287,3 +9288,4 @@ var CdvPurchase;
     }
     CdvPurchase.VerifiedReceipt = VerifiedReceipt;
 })(CdvPurchase || (CdvPurchase = {}));
+});
