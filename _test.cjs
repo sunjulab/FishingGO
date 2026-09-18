ddd8df505cc39ea2a643a28e825ac5e314cadf67
FishingGO Dev <dev@fishinggo.com>
2026-09-18 12:12:23 +0900

    Updates

--- _test.cjs
+++
@@ -1,1 +0,0 @@
-const axios = require('axios'); axios.get('https://d.kbs.co.kr/special/cctvShare?cctvId=9995').then(res => console.log(res.data));
--- fix_jsx.cjs
+++
@@ -1,8 +0,0 @@
-
-const fs = require('fs');
-let mypage = fs.readFileSync('src/pages/MyPage.jsx', 'utf8');
-mypage = mypage.replace('{(() => { const Icon = s.icon; return <Icon size={11} color={s.color} fill={s.color} />; })()}', '{/* eslint-disable-next-line react-hooks/refs */}\n                   {(() => { const Icon = s.icon; return <Icon size={11} color={s.color} fill={s.color} />; })()}');
-mypage = mypage.replace('{(() => { const Icon = n.icon; return <Icon size={18} color=\\'#8E8E93\\' />; })()}', '{/* eslint-disable-next-line react-hooks/refs */}\n                                  {(() => { const Icon = n.icon; return <Icon size={18} color=\\'#8E8E93\\' />; })()}');
-fs.writeFileSync('src/pages/MyPage.jsx', mypage);
-console.log('done');
-
